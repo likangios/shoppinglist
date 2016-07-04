7eac773a4dceea63ff9b3a74d38014a5aa104397
likang <likangios@163.com>
2016-07-04 10:43:47 +0800

    init

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'shoppinglist' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  pod 'MBProgressHUD', '~> 0.9.2'
+  use_frameworks!
+  # Pods for shoppinglist
+
+end
---
+++ shoppinglist/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  shoppinglist
+//
+//  Created by FengLing on 16/6/30.
+//  Copyright © 2016年 lk. All rights reserved.
+//
+
+import UIKit
+
+import MBProgressHUD
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
